659fdf0038073cccadb3746f4931b201c29844d2
Glasgalas <alex.starichenko@icloud.com>
2022-07-29 15:03:57 +0300

    додав частину swaggera

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
 require("dotenv").config();
 
 const usersRouter = require("./routes/api/users");
@@ -14,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
