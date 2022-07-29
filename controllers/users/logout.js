2a216c40291d5a353044598ca09f88c6be19f4d0
Glasgalas <alex.starichenko@icloud.com>
2022-07-29 16:25:24 +0300

    додав авторизацію та логаут

---
+++ controllers/users/logout.js
@@ -0,0 +1,11 @@
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate({ _id }, { token: null });
+  res.status(204).send({
+    status: "success",
+    code: 204,
+    message: `User logout`,
+  });
+};
+
+module.exports = logout;
