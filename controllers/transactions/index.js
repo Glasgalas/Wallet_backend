d92a63ebee224d58735738dc6957e7ef6160ec5c
Glasgalas <alex.starichenko@icloud.com>
2022-07-30 17:14:14 +0300

    add endpoint getCategories

--- controllers/transactions/index.js
+++ controllers/transactions/index.js
@@ -1,5 +1,6 @@
 const add = require("./add");
 const getAll = require("./getAll");
 const remove = require("./remove");
+const getCategories = require("./getCategories");
 
-module.exports = { add, getAll, remove };
+module.exports = { add, getAll, remove, getCategories };
