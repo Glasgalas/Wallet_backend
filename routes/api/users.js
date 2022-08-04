c1f28797bbf7fb581ff86b01ea2222c7204b9dd6
Oleksii Losiev <75750709+alexlex7@users.noreply.github.com>
2022-08-04 15:19:08 +0300

    add get current user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,16 +1,18 @@
-const express = require("express");
-const { users: ctrl } = require("../../controllers");
-const { joiLoginSchema, joiRegSchema } = require("../../models/user");
-const { ctrlWrapper, validation } = require("../../middlewares");
-const { auth } = require("../../middlewares");
+const express = require('express');
+const { users: ctrl } = require('../../controllers');
+const { joiLoginSchema, joiRegSchema } = require('../../models/user');
+const { ctrlWrapper, validation } = require('../../middlewares');
+const { auth } = require('../../middlewares');
 
 const router = express.Router();
 
 //регістрація
-router.post("/register", validation(joiRegSchema), ctrlWrapper(ctrl.register));
+router.post('/register', validation(joiRegSchema), ctrlWrapper(ctrl.register));
 // логін
-router.post("/login", validation(joiLoginSchema), ctrlWrapper(ctrl.login));
+router.post('/login', validation(joiLoginSchema), ctrlWrapper(ctrl.login));
 // логаут
-router.get("/logout", auth, ctrlWrapper(ctrl.logout));
+router.get('/logout', auth, ctrlWrapper(ctrl.logout));
+//getCurrentUser
+router.get('/current', auth, ctrlWrapper(ctrl.getCurrent));
 
 module.exports = router;
